319936995e17a38e5b6ff13ef5ef0ee2a710507e
dendefo <daniek332@gmail.com>
2024-01-29 12:36:23 +0200

    Moving obstacle

--- Assets/Scripts/movingcube.cs
+++ Assets/Scripts/movingcube.cs
@@ -7,26 +7,30 @@ public class movingcube : MonoBehaviour
     [SerializeField] Transform left;
     [SerializeField] Transform right;
     bool isMovingRight;
+    [SerializeField] float TimeToMove;
+    private float lastTimeOfDirectionChange;
     // Start is called before the first frame update
     void Start()
     {
-
+        lastTimeOfDirectionChange = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (isMovingRight)
         {
 
-            transform.Translate(right.position - transform.position);
-            if (Vector3.Distance(transform.position, right.position) < 0.5f) isMovingRight = false;
+            transform.position = Vector3.Lerp(transform.position, right.transform.position, (Time.time - lastTimeOfDirectionChange) / TimeToMove);
+            if (Vector3.Distance(transform.position, right.position) < 0.5f) { isMovingRight = false; lastTimeOfDirectionChange = Time.time; }
         }
 
         else
         {
-            transform.Translate(left.position - transform.position);
-            if (Vector3.Distance(transform.position, left.position) < 0.5f) isMovingRight = true;
+            transform.position = Vector3.Lerp(transform.position, left.transform.position, (Time.time - lastTimeOfDirectionChange) / TimeToMove);
+            if (Vector3.Distance(transform.position, left.position) < 0.5f) { isMovingRight = true; lastTimeOfDirectionChange = Time.time; }
         }
     }
 }
