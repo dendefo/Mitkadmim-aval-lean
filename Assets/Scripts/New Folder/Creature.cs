2fc4fd5f82ecf057b441d76f1488c867976aa346
dendefo <daniek332@gmail.com>
2024-05-03 03:49:03 +0300

    Revamp of project

--- Assets/Scripts/New Folder/Creature.cs
+++ Assets/Scripts/New Folder/Creature.cs
@@ -1,25 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Creature : MonoBehaviour
 {
+    public static event Action<AudioClip> VoiceEvent;
     public static List<Creature> creatures = new();
-    public static Creature chosenCreature;
+    public static List<Creature> chosenCreatures = new();
 
     [SerializeField] CreatureData data;
     public CreatureStats Stats;
     [SerializeField] NavMeshAgent NavAgent;
     [SerializeField] Animator animator;
-    [SerializeField] SkinnedMeshRenderer meshRenderer;
-    private Queue<Vector3> points = new();
+
+    [SerializeField] List<SkinnedMeshRenderer> meshRenderers;
+    private List<Vector3> points = new();
 
 
     private void Awake()
     {
         creatures.Add(this);
         if (data != null) SetStats(data.stats);
+    }
+    private void Update()
+    {
+        UpdateAnimations();
+        if (points.Count > 0 && NavAgent.remainingDistance < 0.1f)
+        {
+            NavAgent.SetDestination(points[0]);
+            var temp = points[0];
+            points.RemoveAt(0);
+            points.Add(temp);
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetFloat("Speed", NavAgent.velocity.magnitude);
+        animator.SetFloat("Rotation", NavAgent.velocity.normalized.x);
     }
 
     public void SetStats(CreatureStats stats)
@@ -28,24 +49,47 @@ public class Creature : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        if (chosenCreature != null) chosenCreature.UnChoise();
+        if (chosenCreatures.Count != 0) chosenCreatures.ForEach(c => c.UnChoise());
+        chosenCreatures.Clear();
         Choise();
 
     }
     public void Choise()
     {
-        chosenCreature = this;
-        foreach (var mat in meshRenderer.materials)
+        if (data.OnPickClip != null) VoiceEvent?.Invoke(data.OnPickClip);
+        chosenCreatures.Add(this);
+        foreach (var renderer in meshRenderers)
         {
-            mat.color = Color.green;
+            renderer.materials.ToList().ForEach(m => m.color = Color.green);
+
         }
+        Debug.Log("Choise", this);
     }
     public void UnChoise()
     {
-        chosenCreature = null;
-        foreach (var mat in meshRenderer.materials)
+        foreach (var renderer in meshRenderers)
         {
-            mat.color = Color.white;
+            renderer.materials.ToList().ForEach(m => m.color = Color.white);
+        }
+    }
+    public void NavigateAndClearPatroll(Vector3 destination)
+    {
+        NavAgent.SetDestination(destination);
+        points.Clear();
+        VoiceEvent?.Invoke(data.OnMoveClip);
+    }
+    public void AddPatrollPoint(Vector3 destination)
+    {
+        points.Add(destination);
+    }
+    public void IsInsideRect(Rect rect, Camera camera)
+    {
+        var screenPos = camera.WorldToScreenPoint(transform.position);
+        if (rect.Contains(screenPos)) Choise();
+        else if (!(Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.LeftShift)) && chosenCreatures.Contains(this))
+        {
+            UnChoise();
+            chosenCreatures.Remove(this);
         }
     }
 }
